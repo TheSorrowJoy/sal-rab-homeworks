8dd8ab39170341e56ae99e25eff03fde11426ff7
The Sorrow <forbiddenx93@gmail.com>
2023-05-19 23:08:20 +0300

    Final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -39,10 +39,10 @@ function sendRequest(name, phone, address, goods, sum) {
     let countOfGoods = goods.length;
 
     for (let i = 0; i < countOfGoods; i +=1) {
-        data.goods.push(goods[i].title);
+        data.goods.push({title: goods[i].title, count: goods[i].count});
     }
 
-    data.order.address = "ул. " + address.street + ",  дом" + address.house + " , " + address.entrance + " подъезд, " + address.floor + " этаж, " + "кв. " + address.flat;
+    data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, " + "кв. " + address.flat;
     data.order.sum =sum;
 
     data.client = `${name}` + " " + `${phone}`;
